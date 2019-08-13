29a468cec28c66c1722d2463587b73348ed336d6
John Harper <jharper1@tulane.edu>
2019-08-13 14:30:25 -0400

    Crystals and Jquery added

---
+++ assets/game.js
@@ -0,0 +1,45 @@
+// Create variables to hold data
+
+
+
+var crystalRed = 0;
+var crystalYellow = 0;
+var crystalGreen = 0;
+var crystalBlue = 0;
+
+var userGuess = 0;
+var randomGuess = 0;
+
+var wins = 0;
+var losses = 0;
+// Create click functions
+
+$("#red").click(function(){
+    alert("test red")
+});
+$("#yellow").click(function() {
+    alert("test yellow")
+});
+$("#green").click(function() {
+    alert("test green")
+});
+$("#blue").click(function() {
+    alert("test blue")
+});
+
+// Build randomization function for crystals and answer
+
+function start (){
+
+
+
+
+
+
+}
+// Create click functions to run the game
+
+// Create scoring rules
+
+// Call the function to start the game
+start ();
