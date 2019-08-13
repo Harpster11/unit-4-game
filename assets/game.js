9bc40ea154af41c2027da2e8365f4eff2bfa5e25
John Harper <jharper1@tulane.edu>
2019-08-13 15:25:04 -0400

    Randomization algoritm working

--- assets/game.js
+++ assets/game.js
@@ -27,19 +27,28 @@ $("#blue").click(function() {
     alert("test blue")
 });
 
-// Build randomization function for crystals and answer
-
-function start (){
-
-
-
-
-
-
+// Build randomization function for crystals and answers
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) +min;
 }
+
+function startGame (){
+
+randomScore = 0
+userGuess = 0  
+
+randomGuess = getRandom(29, 140);
+
+crystalBlue = getRandom(1, 12);
+crystalGreen = getRandom(1, 12);
+crystalRed = getRandom(1, 12);
+crystalGreen = getRandom(1, 12);
+console.log(CrystalRed + " | " + crystalYellow + " | " + crystalGreen + " | " + crystalBlue);
+
+
 // Create click functions to run the game
 
 // Create scoring rules
 
 // Call the function to start the game
-start ();
+startGame ();
