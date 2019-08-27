b7216dbea75b22e9db318c3a141fb87d3717efa2
John Harper <jharper1@tulane.edu>
2019-08-27 12:39:33 -0400

    Mightily Frustrated: My GameOver function isn't being called no matter where I put it. Help!

--- assets/game.js
+++ assets/game.js
@@ -18,6 +18,34 @@ var getRandom = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function gameOver() {
+    console.log("test");
+        // check if player has lost
+    
+        if(userGuess > randomGuess) {
+            alert("You lose! Try again!");
+            console.log("Player Lost");
+    
+            losses++;
+            startGame();
+        }
+    
+        else if (userGuess == randomGuess) {
+            alert("You win!");
+            console.log("Player Won");
+            wins++;
+            startGame();
+        }
+    
+        else {
+            $("#yourScore") = userGuess;
+    
+        }
+    
+        $("#wins").html(wins);
+        $("#losses").html(losses);
+    
+    }
 
 function startGame (){
 
@@ -30,63 +58,48 @@ crystalBlue = getRandom(1, 12);
 crystalYellow = getRandom(1, 12);
 crystalRed = getRandom(1, 12);
 crystalGreen = getRandom(1, 12);
+$("#randomGuess").text(randomGuess);
 console.log(crystalRed + " | " + crystalYellow + " | " + crystalGreen + " | " + crystalBlue);
-
+}
 // Create click functions
+
 
 $("#red").click(function(){
     userGuess = userGuess + crystalRed;
     console.log(userGuess);
+    $("#yourScore").text(userGuess);
+    gameOver();
 });
 $("#yellow").click(function() {
     userGuess = userGuess + crystalYellow;
     console.log(userGuess);
+    $("#yourScore").text(userGuess);
+    gameOver();
 });
 $("#green").click(function() {
     userGuess = userGuess + crystalGreen;
     console.log(userGuess);
+    $("#yourScore").text(userGuess);
+    gameOver();
 });
 $("#blue").click(function() {
     userGuess = userGuess + crystalBlue;
     console.log(userGuess);
+    $("#yourScore").text(userGuess);
+    console.log("gameover", gameOver);
+    gameOver();
 });
 
 
 // Create click functions to run the game
+// debugger;
 
-$("#randomGuess").html(randomGuess);
 
-$("#yourScore").html(userGuess);
+startGame ();
 
-gameOver();
-}
 
-// Create scoring rules
-
-var gameOver = function () {
-
-    // check if player has lost
-
-    if(userGuess > randomGuess) {
-        alert("You lose! Try again!");
-        console.log("Player Lost");
-
-        losses++;
-        startGame();
-    }
-
-    else if (userGuess === randomGuess) {
-        alert("You win!");
-        console.log("Player Won");
-        wins++;
-        startGame();
-    }
-
-    $("#wins").html(wins);
-    $("#losses").html(losses);
-
-}
 
 
 // Call the function to start the game
-startGame ();
+
+
